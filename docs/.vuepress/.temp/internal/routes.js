f225cc29d8ce67c5e2059487235f44afe7ba8001
nchitrak <nchitrakar@ingenico.com>
2025-02-12 15:20:57 +0100

    new version

---
+++ docs/.vuepress/.temp/internal/routes.js
@@ -0,0 +1,7 @@
+export const redirects = JSON.parse("{}")
+
+export const routes = Object.fromEntries([
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/home/naren/Projects/mine/visit/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Home"} }],
+  ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"/home/naren/Projects/mine/visit/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/home/naren/Projects/mine/visit/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+]);
