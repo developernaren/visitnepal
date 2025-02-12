97f2c8bf93ccb19bb4e18976a8c502d2d257f510
nchitrak <nchitrakar@ingenico.com>
2025-02-12 15:41:20 +0100

    something

--- docs/.vuepress/.temp/pages/tour/index.html.js
+++ docs/.vuepress/.temp/pages/tour/index.html.js
@@ -1,3 +1,3 @@
 import comp from "/home/naren/Projects/mine/visit/docs/.vuepress/.temp/pages/tour/index.html.vue"
-const data = JSON.parse("{\"path\":\"/tour/\",\"title\":\"Introduction\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"Mardi Trek\",\"slug\":\"mardi-trek\",\"link\":\"#mardi-trek\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"tour/README.md\"}")
+const data = JSON.parse("{\"path\":\"/tour/\",\"title\":\"Introduction\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"Mardi Trek\",\"slug\":\"mardi-trek\",\"link\":\"#mardi-trek\",\"children\":[]}],\"git\":{\"updatedTime\":1739370915000,\"contributors\":[{\"name\":\"nchitrak\",\"username\":\"nchitrak\",\"email\":\"nchitrakar@ingenico.com\",\"commits\":1,\"url\":\"https://github.com/nchitrak\"}]},\"filePathRelative\":\"tour/README.md\"}")
 export { comp, data }
